f5b1873232c04abd9b51cb80b660ac6c0d459a11
Anton S <geodesist.sushkov@gmail.com>
2022-04-15 00:46:07 +0300

    ExtrusionRoof добавлен

--- RevitAPIPractice4.1/Main.cs
+++ RevitAPIPractice4.1/Main.cs
@@ -7,16 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.ApplicationServices;
 
 namespace RevitAPIPractice4._1
 {
     [Transaction(TransactionMode.Manual)]
     public class Main : IExternalCommand
     {
-        //public Level level1 { get; }
-        //public Level level2 { get; }
-        //public List<Wall> walls { get; set; }=new List<Wall>();  
-
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             Document doc = commandData.Application.ActiveUIDocument.Document;
@@ -39,8 +36,79 @@ namespace RevitAPIPractice4._1
             AddWindow(doc, level1, walls[1]);
             AddWindow(doc, level1, walls[2]);
             AddWindow(doc, level1, walls[3]);
+            AddRoof(doc, level2, walls);
 
             return Result.Succeeded;
+        }
+
+        private void AddRoof(Document doc, Level level2, List<Wall> walls)
+        {
+            RoofType roofType = new FilteredElementCollector(doc)
+            .OfClass(typeof(RoofType))
+            .OfType<RoofType>()
+            .Where(el => el.Name == "Типовой - 400мм")
+            .Where(el => el.FamilyName == "Базовая крыша")
+            .FirstOrDefault();
+
+            double dt = walls[1].Width/2;
+
+            List<XYZ> points = new List<XYZ>();
+            points.Add(new XYZ(-dt, -dt, 0));
+            points.Add(new XYZ(dt, -dt, 0));
+            points.Add(new XYZ(dt, dt, 0));
+            points.Add(new XYZ(-dt, dt, 0));
+            points.Add(new XYZ(-dt, -dt, 0));
+
+
+            Application applcation = doc.Application;
+            #region Create.NewFootPrintRoof метод
+
+            //CurveArray footprint = applcation.Create.NewCurveArray();
+            //for (int i = 0; i < walls.Count; i++)
+            //{
+            //    LocationCurve curve = walls[i].Location as LocationCurve;
+            //    XYZ p1 = curve.Curve.GetEndPoint(0);
+            //    XYZ p2 = curve.Curve.GetEndPoint(1);
+            //    Line line = Line.CreateBound(p1 + points[i], p2 + points[i + 1]);
+            //    footprint.Append(line);
+            //}
+            //ModelCurveArray footPrintToModelCurveMapping = new ModelCurveArray();
+            //FootPrintRoof footprintRoof = doc.Create.NewFootPrintRoof(footprint, level2, roofType, out footPrintToModelCurveMapping);
+            //ModelCurveArrayIterator iterator = footPrintToModelCurveMapping.ForwardIterator();
+            //iterator.Reset();
+            //while (iterator.MoveNext())
+            //{
+            //    ModelCurve modelCurve = iterator.Current as ModelCurve;
+            //    footprintRoof.set_DefinesSlope(modelCurve, true);
+            //    footprintRoof.set_SlopeAngle(modelCurve, 0.5);
+            //}
+            #endregion
+
+            double width = UnitUtils.ConvertToInternalUnits(10000, UnitTypeId.Millimeters);
+            double start = -width / 2 - dt;
+            double end = width / 2 + dt;
+
+            var wallHeigth = walls[0].get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE);
+
+            CurveArray curveArray = new CurveArray();
+            curveArray.Append(Line.CreateBound(new XYZ (16,-10,13.5), new XYZ(16,0,20)));
+            curveArray.Append(Line.CreateBound(new XYZ(16, 0, 20), new XYZ(16, 10, 13.5)));
+
+            CurveArray footprint = applcation.Create.NewCurveArray();
+
+                LocationCurve curve = walls[1].Location as LocationCurve;
+                XYZ p1 = curve.Curve.GetEndPoint(0);
+                XYZ p2 = curve.Curve.GetEndPoint(1);
+
+            using (Transaction tr = new Transaction(doc))
+            {
+                tr.Start("Create ExtrusionRoof");
+                ReferencePlane plane = doc.Create.NewReferencePlane(new XYZ(0, 0, 0), new XYZ(0, 0, 20), new XYZ(0, 20, 0), doc.ActiveView);
+                doc.Create.NewExtrusionRoof(curveArray, plane, level2, roofType, start, end);
+                tr.Commit();
+            }
+            
+
         }
 
         public List<Wall> AddWalls(Document doc, Level level1, Level level2)
